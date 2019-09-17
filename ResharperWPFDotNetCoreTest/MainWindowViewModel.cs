65b4a0f10974bb5ffc19d241ebfcdc3ebc93cfd5
Stephen White <s.white@d3g.com>
2019-09-17 13:11:23 -0400

    Add project files.

---
+++ ResharperWPFDotNetCoreTest/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ResharperWPFDotNetCoreTest
+{
+    public class MainWindowViewModel
+    {
+        public string SomeString { get; set; }
+
+        public MainWindowViewModel()
+        {
+            SomeString = "Hello Jetbrains";
+        }
+    }
+}
