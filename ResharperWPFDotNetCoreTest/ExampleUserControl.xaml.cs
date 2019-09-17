f82e1135d963bb6fc2274628d8b80dcab4ae706a
Stephen White <s.white@d3g.com>
2019-09-17 13:46:01 -0400

    Added the Nuget Package that causes havok with Resharper

---
+++ ResharperWPFDotNetCoreTest/ExampleUserControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ResharperWPFDotNetCoreTest
+{
+    /// <summary>
+    /// Interaction logic for UserControl.xaml
+    /// </summary>
+    public partial class ExampleUserControl : UserControl
+    {
+        public ExampleUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ResharperWPFDotNetCoreTest/MainWindowViewModel.cs
+++ ResharperWPFDotNetCoreTest/MainWindowViewModel.cs
@@ -6,7 +6,7 @@
 
         public MainWindowViewModel()
         {
-            SomeString = "Hello Jetbrains";
+            SomeString = "Hello Jetbrains!";
         }
     }
 }
